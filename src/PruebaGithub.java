4ed87d22e962fd9e6c1865395c4d011fd93d3d65
Ignacio1997 <nacho1554@hotmail.com>
2016-10-14 20:00:35 +0200

    Initial commit

---
+++ src/PruebaGithub.java
@@ -0,0 +1,6 @@
+
+ * Created by 46420777S on 14/10/2016.
+ */
+public class PruebaGithub {
+    sout
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello wodld mi primer proyecto java en github");
+        System.out.println("ggwp");
+
+    }
+}
